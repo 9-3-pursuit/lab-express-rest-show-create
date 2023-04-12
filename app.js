3249506d3809aab4e8201dc15ec6e080b9b638d7
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-11 22:02:57 -0400

    display all logs

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const app = express();
 
+const logsController = require("./controllers/logController.js");
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("welcome to the captain's log");
 });
+
+app.use("/logs", logsController);
 
 app.get("*", (req, res) => {
   res.status(404).json({ error: "Sorry, no page found!" });
