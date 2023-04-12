0f12116cc02d597874f87fc8a9e3c410351d7358
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-11 22:07:19 -0400

    show logs

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const logs = express.Router();
 const logsArray = require("../models/log.js");
@@ -7,5 +6,14 @@ logs.get("/", (req, res) => {
   res.json(logsArray);
 });
 
+// SHOW logs
+
+logs.get("/:arrayIndex", (req, res) => {
+  if (logsArray[req.params.arrayIndex]) {
+    res.json(logsArray[req.params.arrayIndex]);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
 
 module.exports = logs;
