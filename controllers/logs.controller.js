2e13cc17fce6a80acb009e83f15bb7d367c83665
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-12 18:50:20 -0400

    Adds express paths/methods

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -6,4 +6,41 @@ logRouter.get("/", (req, res) => {
   res.send(logArray);
 });
 
+logRouter.post("/", (req, res) => {
+  logArray.push(req.body);
+  res.status(201).send();
+});
+
+logRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const isValidIndex = logArray[id] !== undefined;
+  if (isValidIndex) {
+    res.send(logArray[id]);
+  } else {
+    res.redirect();
+  }
+});
+
+logRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const isValidIndex = logArray[id] !== undefined;
+  if (isValidIndex) {
+    logArray[id] = req.body;
+    res.status(202).send();
+  } else {
+    res.redirect();
+  }
+});
+
+logRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const isValidIndex = logArray[id] !== undefined;
+  if (isValidIndex) {
+    logArray.splice(id, 1);
+    res.status(200).send(logArray);
+  } else {
+    res.redirect();
+  }
+});
+
 module.exports = logRouter;
