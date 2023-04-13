2ce6b40fc0bb1820a0b5f6efb060f9e6721b12bb
touhami-ben <brooklyn.ben76@gmail.com>
2023-04-12 23:30:27 -0400

    add delete and update routes and some error handling

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,24 +1,50 @@
 const express = require("express");
 const logs = express.Router();
 const logsArray = require("../models/log");
-
+const { logsValidator } = require("../models/validators.js");
+//index-all logs
 logs.get("/", (req, res) => {
   res.json(logsArray);
 });
-// SHOW
-logs.get("/:id", (req, res) => {
-    const { id } = req.params;
-    if (logsArray[id]) {
-      res.json(logsArray[id]);
+// SHOW - get one
+logs.get("/:index", (req, res) => {
+    const { index } = req.params;
+    if (logsArray[index]) {
+      res.json(logsArray[index]);
     } else {
       res.redirect("/*");
     }
   });
   // CREATE
-logs.post("/", (req, res) => {
+logs.post("/", logsValidator, (req, res) => {
     logsArray.push(req.body);
     res.json(logsArray[logsArray.length - 1]);
   });
+
+  //UPDATE 
+   logs.put("/:index", (req, res) => {
+    const { index } = req.params;
+if(logsArray[index]){
+    logsArray[index] = req.body;
+    res.status(200).json(logsArray[index]);
+     
+} else {
+    res.status(404).json({error: "Not Found"});
+}
+});
+//delete 
+logs.delete("/:index", (req, res) => {
+    const { index } = req.params;
+    //const index = req.params.index(destructions)
+    if (logsArray[index]){
+        const deletedLog = logsArray.splice(index, 1);//return deleted log
+        res.status(200).json(logsArray);
+    } else {
+        res.status(404).json({error: "Not Found"});
+    }
+});
+
+
   
   
 
--- models/validators.js
+++ models/validators.js
@@ -1,7 +1,10 @@
 const logsValidator = (req, res, next) => {
     if(req.body.hasOwnProperty("captainName") && req.body.hasOwnProperty("title")){
         next()
-    }else {
-        res.status(400).json({error: "logs must contain a captainName and a title"})    }
+    } else if(!req.body.captainName || !req.body.title){
+        return res.status(400).json({error: "logs cannot contain empty string"})
+    }
+    else {
+        return res.status(400).json({error: "logs must contain a captainName and a title"})    }
 };
  module.exports = { logsValidator };
