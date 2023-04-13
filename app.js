6d821b17325f96236e6d2bf0c3014405e1ecc204
Keeanu Rodriguez <keeanu7@gmail.com>
2023-04-13 00:18:02 -0400

    added update/put

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const logController = require("./controllers/logs.js");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("This code runs for every request");
+    next();
+});
 
 app.get("/", (req, res) => {
     res.send("welcome to the captain's log");
 })
+
+app.use("/logs", logController);
 
 app.get("*", (req, res) => {
     res.status(404).send("Error")
