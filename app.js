0bb228926e57e22a8719fdf75ffa5ede3c23a15f
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-15 08:53:17 -0400

    Adds cors to api

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const logRouter = require("./controllers/logs.controller");
 app.use(express.json());
+app.use(cors());
 app.use("/logs", logRouter);
 
 app.get("/", (req, res) => {
