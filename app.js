e08f2900db3d52dcb2f7228c183cf5bead0d03c2
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-15 19:59:23 -0400

    imported cors correctly

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // DEPENDENCIES
 const express = require("express");
+const cors = require("cors");
 
 // CONFIGURATION
 const app = express();
-
+app.use(cors());
 // const logsArray = require("./models/log.js");
 const logsControllerArr = require('./controllers/logsController.js');
 
 app.use(express.json()) // for parsing application/json
 // app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+,
 app.use((req, res, next) => {;
   next();
 });
