f4f6ca20eaa6999f8c78d71d1bd5551d6fbe68cd
Wilton <wiltoncamilol@gmail.com>
2023-04-15 18:54:35 -0400

    Attempt 1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+
+ const logsController = require("./controllers/logsController.js");
+
+
+
+// "/" is equal to http://localhost:3333
+app.get("/",(req,res)=> {
+    res.send("Hello Welcome")
+})
+
+app.use(express.json())
+
+// "/logs" is equal to http://localhost:3333/logs
+app.use("/logs", logsController);     
+
+app.get("*", (req,res) => {
+    res.status(400).json("Page Not Found")
+})
+
+module.exports = app;
