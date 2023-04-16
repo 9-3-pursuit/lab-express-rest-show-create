6c006994d47459fed2753e012a861108f9c49204
touhami-ben <brooklyn.ben76@gmail.com>
2023-04-16 03:36:32 -0400

    add cors middleware to allow API requests from any app

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // DEPENDENCIES
 const express = require("express");
 const logsController = require("./controllers/logsController");
+const cors = require("cors");
 
 //CONFIGURATION
 const app = express();
 //middleware
 
 app.use(express.json());//parse incoming json
+app.use(cors());
 // ROUTES
 app.get("/", (req, res) => {
   res.send("welcome to the captain's log");
